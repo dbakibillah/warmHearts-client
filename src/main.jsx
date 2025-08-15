8420f3be8e3e1ca91f4943eb9df9d71895f4fe2f
dbakibillah <bakibillah.dev@gmail.com>
2025-08-16 00:51:26 +0600

    project setup & navbar done

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,22 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Root from "./routes/Root";
 import Home from "./pages/home/Home";
+import AuthProvider from "./providers/AuthProviders";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Root />}>
-                <Route path="/" element={<Home />} />
-                {/* <Route path="/login" element={<Login />} /> */}
-            </Route>
-        </Routes>
+        <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <Routes>
+                    <Route path="/" element={<Root />}>
+                        <Route path="/" element={<Home />} />
+                        {/* <Route path="/login" element={<Login />} /> */}
+                    </Route>
+                </Routes>
+            </QueryClientProvider>
+        </AuthProvider>
     </BrowserRouter>
 );
